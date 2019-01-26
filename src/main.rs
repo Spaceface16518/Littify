8d9f569f01dee535f64bb1c7577ca3b40eb44d3c
Spaceface16518 <33424649+Spaceface16518@users.noreply.github.com>
2019-01-26 16:14:56 -0600

    Implement ProccessArgsExt trait

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
 extern crate littify_core;
-use littify_core::{littify_string, process_args};
+use littify_core::{littify_string, ProcessArgsExt};
 use std::env::args;
 
 fn main() {
     println!(
         "{}",
-        littify_string(process_args(args().skip(1).collect::<Vec<String>>()))
+        littify_string(args().skip(1).collect::<Vec<String>>().process_args())
             .as_str()
     );
 }
