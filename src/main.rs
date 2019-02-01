f722f8d4bea80264f26427d6ffdc00483a6ead18
Spaceface16518 <33424649+Spaceface16518@users.noreply.github.com>
2019-02-01 00:15:46 -0600

    Implement LittifyStringExt in main bin

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,7 @@
 extern crate littify_core;
-use littify_core::{littify_string, ProcessArgsExt};
+use littify_core::{LittifyStringExt, ProcessArgsExt};
 use std::env::args;
 
 fn main() {
-    println!(
-        "{}",
-        littify_string(args().skip(1).process_args())
-            .as_str()
-    );
+    println!("{}", &args().skip(1).process_args().littify());
 }
